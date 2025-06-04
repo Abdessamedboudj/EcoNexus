0b711b4cf249a526962f4de3800a96b03f8bcb40
Abdessamed Boudjerada <132408199+Abdessamedboudj@users.noreply.github.com>
2025-06-04 17:32:53 +0100

    Update Vercel configuration and dependencies

---
+++ vercel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p static
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Make migrations
+python manage.py makemigrations
+python manage.py migrate 
